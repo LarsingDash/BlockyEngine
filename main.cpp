98dd274ec4a41df3c8262369b47e6de6a06358b9
Kampf <hmkamphof@gmail.com>
2024-09-12 16:04:27 +0200

    audio test not working yet

--- main.cpp
+++ main.cpp
@@ -1,168 +1,88 @@
 ﻿#include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_audio.h> // Use SDL_mixer for audio handling
 
 #include <iostream>
 #include <cmath>
 
-//Methods
-void input(SDL_Scancode key);
-void init();
-void draw();
-void update(float delta);
-void destroy();
+// https://www.libsdl.org/release/SDL-1.2.15/docs/html/guideaudioexamples.html
 
-//SDL
-SDL_Window* window;
-SDL_Renderer* renderer;
-bool shouldQuit = false;
+static Uint8 *audio_chunk;
+static Uint32 audio_len;
+static Uint8 *audio_pos;
 
-//Window
-int windowWidth = 600;
-int windowHeight = 800;
-float windowWidthF = (float) windowWidth;
-float windowHeightF = (float) windowHeight;
+/* The audio function callback takes the following parameters:
+   stream:  A pointer to the audio buffer to be filled
+   len:     The length (in bytes) of the audio buffer
+*/
+void fill_audio(void *udata, Uint8 *stream, int len)
+{
+    /* Only play if we have data left */
+    if (audio_len == 0)
+        return;
 
-//Box
-float x = 0, y = 0;
-SDL_Texture* boxImage;
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-	std::cout << "Launching" << std::endl;
-
-	//Initialize video from SDL
-	if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0) {
-		std::cerr << "Couldn't init video: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-	if (IMG_Init(IMG_INIT_PNG) < 0) {
-		std::cerr << "Couldn't init images: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-
-	//Create window
-	window = SDL_CreateWindow("SDLTest", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-							  windowWidth, windowHeight,
-							  SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		std::cerr << "Couldn't create window: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	//Renderer
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (renderer == nullptr) {
-		std::cerr << "Couldn't create renderer: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		SDL_DestroyWindow(window);
-		return 1;
-	}
-
-	//Init
-	init();
-
-	//Main loop vars
-	SDL_Event event;
-
-	Uint32 prevTicks = SDL_GetTicks();
-	Uint32 frameCount = 0;
-	Uint32 fps = 0;
-	Uint32 fpsInterval = 1000;
-
-	//Main loop
-	while (!shouldQuit) {
-		//Fps delta
-		Uint32 curTicks = SDL_GetTicks();
-		Uint32 delta = curTicks - prevTicks;
-		prevTicks = curTicks;
-		
-		//Go through the queue of events (0 if there are none)
-		while (SDL_PollEvent(&event) != 0) {
-			switch (event.type) {
-				//Quit if event type is quit
-				case SDL_QUIT:
-					shouldQuit = true;
-					break;
-
-				case SDL_KEYDOWN:
-					input(event.key.keysym.scancode);
-					break;
-
-				default:
-					break;
-			}
-		}
-
-		//Cycle
-		update(static_cast<float>(delta) / 1000.f);
-		draw();
-
-		//Fps count
-		frameCount++;
-		fps += delta;
-		if (fps >= fpsInterval) {
-			std::cout << "FPS: " << frameCount << std::endl;
-			fps = 0;
-			frameCount = 0;
-		}
-	}
-
-	//Cleanup
-	destroy();
-	std::cout << "Quit" << std::endl;
-
-	return 0;
+    /* Mix as much data as possible */
+    len = (len > audio_len ? audio_len : len);
+    SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
+    audio_pos += len;
+    audio_len -= len;
 }
 
-void init() {
-	SDL_RenderSetVSync(renderer, 1);
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        std::cerr << "Couldn't initialize SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-	SDL_Surface* surface = IMG_Load("../assets/triangle.png");
-	boxImage = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
+    SDL_AudioSpec wanted;
+    // extern void fill_audio(void *udata, Uint8 *stream, int len);
 
-}
+    /* Set the audio format */
+    wanted.freq = 22050;
+    wanted.format = AUDIO_S16SYS;
+    wanted.channels = 2;    /* 1 = mono, 2 = stereo */
+    wanted.samples = 1024;  /* Good low-latency value for callback */
+    wanted.callback = fill_audio;
+    wanted.userdata = NULL;
 
-void input(SDL_Scancode key) {
-	switch (key) {
-		default:
-			break;
-		case SDL_SCANCODE_ESCAPE:
-			shouldQuit = true;
-			break;
-	}
-}
+    /* Open the audio device, forcing the desired format */
+    if (SDL_OpenAudio(&wanted, NULL) < 0) {
+        std::cerr << "Couldn't open audio: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
 
-const float pi = 3.14159265f * 2.f;
+    /* Load the audio data ... */
+    // Here you should load your audio data into audio_chunk
+    // For example, using SDL_LoadWAV or another method to load audio data
+    // For demonstration, let's assume audio_chunk is loaded and audio_len is set
 
-void update(float delta) {
-	x += 1 * delta;
-	if (x >= pi) x -= pi;
+    // Example loading audio data (replace with actual loading code)
+    SDL_AudioSpec wav_spec;
+    printf ("D:/GitHub/BlockyEngine/applause_y.wav,  %d,  %d", *audio_chunk, audio_len);
+    if (SDL_LoadWAV("D:/GitHub/BlockyEngine/applause_y.wav", &wav_spec, &audio_chunk, &audio_len) == NULL) {
+        std::cerr << "Couldn't load audio file: " << SDL_GetError() << std::endl;
+        SDL_CloseAudio();
+        SDL_Quit();
+        return 1;
+    }
 
-	y += 1 * delta;
-	if (y >= pi) y -= pi;
-}
+    audio_pos = audio_chunk;
 
-void draw() {
-	SDL_SetRenderDrawColor(renderer, 25, 25, 25, SDL_ALPHA_OPAQUE);
-	SDL_RenderClear(renderer);
+    /* Let the callback function play the audio chunk */
+    SDL_PauseAudio(0);
 
-	float size = 50;
-	SDL_FRect rect{(sinf(x) + 1.f) / 2.f * (windowWidthF - size),
-				   (cosf(y) * -1 + 1.f) / 2.f * (windowHeightF - size),
-				   size, size};
+    /* Wait for sound to complete */
+    while (audio_len > 0) {
+        SDL_Delay(100);         /* Sleep 1/10 second */
+    }
 
-	SDL_RenderCopyExF(renderer, boxImage, nullptr, &rect, (x / (pi) * 360) + 90, nullptr, SDL_RendererFlip::SDL_FLIP_NONE);
+    // Clean up
+    SDL_FreeWAV(audio_chunk);
+    SDL_CloseAudio();
+    SDL_Quit();
 
-	SDL_RenderPresent(renderer);
-}
-
-void destroy() {
-	SDL_DestroyTexture(boxImage);
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
-	IMG_Quit();
+    return 0;
 }

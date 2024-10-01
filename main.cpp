6ec6ea286dbb888bf49bb4a86f2344c79704f256
Kampf <hmkamphof@gmail.com>
2024-10-01 10:11:00 +0200

    no 2 audio files playback at the same time

--- main.cpp
+++ main.cpp
@@ -4,33 +4,43 @@
 
 #include "playAudioWavFile.hpp"
 
+/*
+ * requirments
+ * async non blocking audio playback
+ *   send and forget
+ * multiple audio sorces simultanias play back
+ * ( same instance audio, so no replay, but new instance every time audio chould be played, so no playback is stoped and restarted)
+ *
+ *
+*/
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-    playAudioWavFile wavFile;
+    playAudioWavFile countDown;
+    //playAudioWavFile applause;
 
     AudioReturn audioReturn;
 
-    //audioReturn = wavFile.loadAudioWavFile("D:/GitHub/BlockyEngine/applause_y.wav");
-    //if (audioReturn != SUCCES){ return audioReturn;}
-    audioReturn = wavFile.loadAudioWavFile("D:/GitHub/BlockyEngine/countDown.wav");
+    audioReturn = countDown.loadAudioWavFile("D:/GitHub/BlockyEngine/countDown.wav");
     if (audioReturn != SUCCES){ return audioReturn;}
+    //audioReturn = applause.loadAudioWavFile("D:/GitHub/BlockyEngine/applause_y.wav");
+    //if (audioReturn != SUCCES){ return audioReturn;}
 
-    wavFile.playAudio();
+    countDown.playAudio();
+    //applause.playAudio();
 
     printf("playing audio 2x\n");
 
-
-    wavFile.playAudio();
-
-    int milliseconds = 5000;
+    int milliseconds = 3000;
     Sleep(milliseconds);
 
-    wavFile.rewindAudio();
+    countDown.rewindAudio();
+    //applause.rewindAudio();
 
-    wavFile.playAudio();
+    countDown.playAudio();
+    //applause.playAudio();
 
-    milliseconds = 7000;
+    milliseconds = 2000;
     Sleep(milliseconds);
 
     return 0;

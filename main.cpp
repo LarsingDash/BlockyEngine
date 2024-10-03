d0fad578f3bd0c6860857d970964c51fdd2868c0
Kampf <hmkamphof@gmail.com>
2024-10-03 09:24:49 +0200

    ;

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 static const char *COUNT_DOWN = "../AudioFiles/countDown.mp3";
 static const char *APPLAUSE = "../AudioFiles/applause_y.wav";
 
-int main(int argc, char **argv) {
+void Test_Play() {
     PlayAudioFile audio1;
     audio1.loadAudioFile(COUNT_DOWN);
 
@@ -27,5 +27,42 @@ int main(int argc, char **argv) {
 
     audio2.playAudio(1);
     Sleep(7000);
+}
+
+int main(int argc, char **argv) {
+
+
+
+    PlayAudioFile audio;
+    audio.loadAudioFile("../AudioFiles/1.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/2.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/3.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/4.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/5.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/6.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/7.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/8.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/9.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
+    audio.loadAudioFile("../AudioFiles/10.mp3");
+    audio.playAudio(0);
+    Sleep(1000);
     return 0;
 }

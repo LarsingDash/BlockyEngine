6978883a39825afdbc9def5b062fa657c0171477
Kampf <hmkamphof@gmail.com>
2024-11-13 10:21:24 +0100

    [fix] create oneTestClass to call Test

---
+++ engine/Testing/BLoggerTest.cpp
@@ -0,0 +1,37 @@
+//
+// Created by hmkam on 13/11/2024.
+//
+
+#include "BLoggerTest.hpp"
+
+#include "../../engine/logging/BLogger.hpp"
+#include <string>
+
+void TestBLoggerFunc(std::string str) {
+	BLOCKY_ENGINE_DEBUG(str)
+}
+
+class BloggerClass {
+	public:
+		static void FuncBloggerClass(std::string str) {
+			BLOCKY_ENGINE_DEBUG(str)
+		}
+};
+
+void BLoggerTest::Test() {
+	BLOCKY_ENGINE_DEBUG("Debugging information, in BLoggerTest::Test()");
+
+	for (int i = 0; i < 5; i++) {
+		BLOCKY_ENGINE_INFO("Program update: " + std::to_string(i));
+
+		//delay
+		for (int j = 0; j < 100000000; j++) { if (i == j) { j = i; } }
+	}
+
+	BLOCKY_ENGINE_ERROR("An error occurred.");
+	BLOCKY_ENGINE_WARNING("An warning occurred.");
+
+	TestBLoggerFunc("Debugging information, in TestBLoggerFunc()");
+
+	BloggerClass::FuncBloggerClass("Debugging information, in BloggerClass::FuncBloggerClass()");
+}
--- main.cpp
+++ main.cpp
@@ -1,24 +1,5 @@
-﻿#include "engine/TestClass.hpp"
-#include "./engine/logging/BLogger.hpp"
-
-void testFuncInMain(std::string str) {
-	BLOCKY_ENGINE_DEBUG(str)
-}
+﻿#include "engine/Testing/BLoggerTest.hpp"
 
 int main() {
-	BLOCKY_ENGINE_DEBUG("Debugging information.");
-	// Example usage of the logger
-	for (int i = 0; i < 5; i++) {
-		BLOCKY_ENGINE_INFO("Program update: " + std::to_string(i));
-
-		//delay
-		for (int j = 0; j < 1000000; j++) { if (i == j) { j = i; } }
-	}
-
-	BLOCKY_ENGINE_ERROR("An error occurred.");
-	BLOCKY_ENGINE_WARNING("An warning occurred.");
-
-	testFuncInMain("test main func");
-
-	TestClass::DoTestAndLog();
+	BLoggerTest::Test();
 }

df732d3b41fc1237f6e8a9cbc7fa4608a6f07b52
Oliver Norton <o.k.norton21@gmail.com>
2024-11-19 16:52:52 +0100

    Animation struct

--- engine/components/animation/Animation.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 11896 on 19/11/2024.
-//
-
-#include "Animation.hpp"
--- engine/components/animation/Animation.hpp
+++ engine/components/animation/Animation.hpp
@@ -5,8 +5,18 @@
 #ifndef BLOCKYENGINE_ENGINE_COMPONENTS_ANIMATION_ANIMATION_HPP_
 #define BLOCKYENGINE_ENGINE_COMPONENTS_ANIMATION_ANIMATION_HPP_
 
-class Animation {
+#include <vector>
+#include <string>
 
+struct Animation {
+	std::string tag;
+	bool isLooping;
+	std::vector<float> frameBoundingBox;
+	std::pair<int, int> animationRange;
+
+	Animation(std::string tag, bool looping, std::pair<int, int> range, std::vector<float> boundingBox)
+			: tag(std::move(tag)), isLooping(looping),
+			  animationRange(std::move(range)), frameBoundingBox(std::move(boundingBox)) {}
 };
 
 #endif //BLOCKYENGINE_ENGINE_COMPONENTS_ANIMATION_ANIMATION_HPP_

21963ca24fc1ae6c7e56946debafbc2387b8da95
LarsingDash <lars.villevoye@gmail.com>
2024-11-26 13:39:55 +0100

    Merged Relative Transforms and Animations

--- engine/sceneManager/SceneManager.cpp
+++ engine/sceneManager/SceneManager.cpp
@@ -15,6 +15,7 @@ SceneManager::SceneManager() :
 		recalculationList() {
 	recalculationList.reserve(25);
 
+	//Relativity
 	testScene->transform->SetPosition(250, 250);
 	auto& row = testScene->AddChild("TopRow");
 	row.transform->SetRotation(10);
@@ -36,6 +37,23 @@ SceneManager::SceneManager() :
 	auto& test = tRRight.AddChild("Test");
 	test.transform->SetRotation(15);
 	test.AddComponent<RectangleRenderable>("TestR", glm::vec4{0, 0, 255, 255});
+
+	//Animations
+	auto& animatedObject = testScene->AddChild("AnimatedObject");
+	animatedObject.transform->SetScale(200.f, 200.f);
+	animatedObject.transform->SetPosition(100.f, 100.f);
+	animatedObject.transform->SetRotation(35);
+
+	auto& animatedSprite = animatedObject.AddComponent<AnimationRenderable>(
+			"animTag", "../assets/character_spritesheet.png",
+			"spriteTag", 32, 32
+	);
+	auto& animationController = animatedObject.AddComponent<AnimationController>("animControllerTag", animatedSprite);
+
+	animationController.AddAnimation("idle", 0, 7, 0.1f, true);
+	animationController.AddAnimation("run", 8, 15, 0.1f, true);
+
+	animatedObject.GetComponent<AnimationController>()->PlayAnimation("idle");
 }
 
 void SceneManager::Update(float delta) {

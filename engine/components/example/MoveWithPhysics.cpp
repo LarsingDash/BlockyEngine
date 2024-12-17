6c00563813986a418d60ae7b8c7fc0a813c62471
Dogukan-lab <57491366+Dogukan-lab@users.noreply.github.com>
2024-12-17 21:09:01 +0100

    [ADD] Added example component for physics based movement

---
+++ engine/components/example/MoveWithPhysics.cpp
@@ -0,0 +1,39 @@
+//
+// Created by doguk on 12/17/2024.
+//
+
+#include "MoveWithPhysics.hpp"
+
+#include <gameObject/GameObject.hpp>
+
+MoveWithPhysics::MoveWithPhysics(GameObject* gameObject, const char* tag, PhysicsBody* body): Component(gameObject, tag), _body(body)
+{
+}
+
+MoveWithPhysics::MoveWithPhysics(const MoveWithPhysics& other): Component(other.gameObject, other.tag.c_str()), _body(std::make_unique<PhysicsBody>(*other._body))
+{
+}
+
+//NOTE: This is ugly never actually do this.
+glm::vec2 direction;
+
+void MoveWithPhysics::Start()
+{
+    direction = _body->GetTypeProperties().velocity;
+    // _body = std::make_unique<PhysicsBody>(*gameObject->GetComponent<PhysicsBody>("BoxColl"));
+}
+
+void MoveWithPhysics::Update(float delta)
+{
+    direction += glm::vec2{delta, 0};
+    _body->GetTypeProperties().SetVelocity(direction);
+}
+
+void MoveWithPhysics::End()
+{
+}
+
+Component* MoveWithPhysics::_clone(const GameObject& parent)
+{
+    return new MoveWithPhysics(*this);
+}
---
+++ engine/components/example/MoveWithPhysics.hpp
@@ -0,0 +1,30 @@
+//
+// Created by doguk on 12/17/2024.
+//
+
+#ifndef MOVEWITHPHYSICS_HPP
+#define MOVEWITHPHYSICS_HPP
+#include <components/physics/PhysicsBody.hpp>
+
+#include "components/Component.hpp"
+
+class MoveWithPhysics : public Component {
+public:
+    MoveWithPhysics(GameObject* gameObject, const char* tag, PhysicsBody* body);
+    ~MoveWithPhysics() override = default;
+
+    MoveWithPhysics(const MoveWithPhysics& other);
+
+    void Start() override;
+    void Update(float delta) override;
+    void End() override;
+
+private:
+    Component* _clone(const GameObject& parent) override;
+
+    std::unique_ptr<PhysicsBody> _body;
+};
+
+
+
+#endif //MOVEWITHPHYSICS_HPP
